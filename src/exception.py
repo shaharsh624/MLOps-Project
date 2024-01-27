f955b6dcc2bb9a75b457890112dcfbef9dd07136
Harsh Shah <harshdev624@gmail.com>
2024-01-27 08:37:22 +0530

    Did Data Transformation

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -10,10 +11,11 @@ def error_message_detail(error, error_detail: sys):
         )
     )
 
+    logging.error(error_message)
     return error_message
 
 
-class CustomeException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
